651fc1aee0d471bd1aa1a4060b4f3f3025b12755
danutzcodrescu <danutzcodrescu@gmail>
2020-10-03 10:22:29 +0200

    wip: seems that computed field was missing in hasura cloud

--- functions/refresh_user_token/refresh_user_token.js
+++ functions/refresh_user_token/refresh_user_token.js
@@ -20,9 +20,7 @@ exports.handler = async (event, context) => {
     return { statusCode: 400, body: 'cannot parse function body' };
   }
   try {
-    console.log(request);
     const user = await graphql(userQuery, { pk: request.user });
-    console.log(user.data.errors);
     if (user.data.data.users_by_pk.expired === 0) {
       return {
         statusCode: 200,
