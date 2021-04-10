eec3133f2a27da247d0231d6c0a0bb9505ad75e5
Danut Codrescu <danutzcodrescu@gmail.com>
2021-04-10 15:32:05 +0200

    chore: test crashing function

--- functions/authenticate-user/authenticate-user.js
+++ functions/authenticate-user/authenticate-user.js
@@ -22,52 +22,55 @@ exports.handler = async (event, context) => {
 
   try {
     console.log('test');
-    const {
-      athlete,
-      refresh_token,
-      token_type,
-      expires_at,
-      access_token,
-    } = await authenticateOnStrava(request.code);
-    console.log(JSON.stringify(athlete));
-    const existingUser = await checkForExistingUser(
-      athlete.id,
-      access_token,
-      refresh_token,
-      expires_at
-    );
-    if (existingUser) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          external_id: athlete.id,
-          access_token,
-        }),
-      };
-    }
-
-    const athleteProfile = await getAthleteProfile(token_type, access_token);
-    const variables = {
-      external_id: athlete.id,
-      access_token,
-      refresh_token,
-      scope: request.scope,
-      ...athleteProfile,
-    };
-    const user = await graphql(insertUserMutation, {
-      object: variables,
-    });
-    const {
-      data: { insert_users_one },
-    } = user.data;
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        access_token: insert_users_one.access_token,
-        external_id: insert_users_one.external_id,
-        expires_at,
-      }),
     };
+    // const {
+    //   athlete,
+    //   refresh_token,
+    //   token_type,
+    //   expires_at,
+    //   access_token,
+    // } = await authenticateOnStrava(request.code);
+    // console.log(JSON.stringify(athlete));
+    // const existingUser = await checkForExistingUser(
+    //   athlete.id,
+    //   access_token,
+    //   refresh_token,
+    //   expires_at
+    // );
+    // if (existingUser) {
+    //   return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({
+    //       external_id: athlete.id,
+    //       access_token,
+    //     }),
+    //   };
+    // }
+
+    // const athleteProfile = await getAthleteProfile(token_type, access_token);
+    // const variables = {
+    //   external_id: athlete.id,
+    //   access_token,
+    //   refresh_token,
+    //   scope: request.scope,
+    //   ...athleteProfile,
+    // };
+    // const user = await graphql(insertUserMutation, {
+    //   object: variables,
+    // });
+    // const {
+    //   data: { insert_users_one },
+    // } = user.data;
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify({
+    //     access_token: insert_users_one.access_token,
+    //     external_id: insert_users_one.external_id,
+    //     expires_at,
+    //   }),
+    // };
   } catch (err) {
     console.log(err?.data?.errors);
     return { statusCode: 500, body: err.toString() };
