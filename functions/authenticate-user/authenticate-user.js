bce5052051f776b550b6ba3e223b269f266aa46a
Danut Codrescu <danutzcodrescu@gmail.com>
2021-04-10 16:27:49 +0200

    test failing function in netlify

--- functions/authenticate-user/authenticate-user.js
+++ functions/authenticate-user/authenticate-user.js
@@ -16,65 +16,63 @@ exports.handler = async (event, context) => {
   let request;
   try {
     request = JSON.parse(event.body);
-    console.log(JSON.stringify(request));
-    console.log(process.env.HASURA_ADMIN_SECRET);
-    return {
-      statusCode: 200,
-      body: 'test',
-    };
   } catch (e) {
     return { statusCode: 400, body: 'cannot parse function body' };
   }
 
-  // try {
-  //   const {
-  //     athlete,
-  //     refresh_token,
-  //     token_type,
-  //     expires_at,
-  //     access_token,
-  //   } = await authenticateOnStrava(request.code);
-  //   console.log(JSON.stringify(athlete));
-  //   const existingUser = await checkForExistingUser(
-  //     athlete.id,
-  //     access_token,
-  //     refresh_token,
-  //     expires_at
-  //   );
-  //   if (existingUser) {
-  //     return {
-  //       statusCode: 200,
-  //       body: JSON.stringify({
-  //         external_id: athlete.id,
-  //         access_token,
-  //       }),
-  //     };
-  //   }
+  try {
+    const {
+      athlete,
+      refresh_token,
+      token_type,
+      expires_at,
+      access_token,
+    } = await authenticateOnStrava(request.code);
+    console.log(JSON.stringify(athlete));
+    // const existingUser = await checkForExistingUser(
+    //   athlete.id,
+    //   access_token,
+    //   refresh_token,
+    //   expires_at
+    // );
+    // if (existingUser) {
+    //   return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({
+    //       external_id: athlete.id,
+    //       access_token,
+    //     }),
+    //   };
+    // }
+    return {
+      statusCode: 200,
+      body: 'test',
+    };
 
-  //   const athleteProfile = await getAthleteProfile(token_type, access_token);
-  //   const variables = {
-  //     external_id: athlete.id,
-  //     access_token,
-  //     refresh_token,
-  //     scope: request.scope,
-  //     ...athleteProfile,
-  //   };
-  //   const user = await graphql(insertUserMutation, {
-  //     object: variables,
-  //   });
-  //   const {
-  //     data: { insert_users_one },
-  //   } = user.data;
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify({
-  //       access_token: insert_users_one.access_token,
-  //       external_id: insert_users_one.external_id,
-  //       expires_at,
-  //     }),
-  //   };
-  // } catch (err) {
-  //   console.log(err?.data?.errors);
-  //   return { statusCode: 500, body: err.toString() };
-  // }
+    // const athleteProfile = await getAthleteProfile(token_type, access_token);
+    // const variables = {
+    //   external_id: athlete.id,
+    //   access_token,
+    //   refresh_token,
+    //   scope: request.scope,
+    //   ...athleteProfile,
+    // };
+    // const user = await graphql(insertUserMutation, {
+    //   object: variables,
+    // });
+    // const {
+    //   data: { insert_users_one },
+    // } = user.data;
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify({
+    //     access_token: insert_users_one.access_token,
+    //     external_id: insert_users_one.external_id,
+    //     expires_at,
+    //   }),
+    // };
+  } catch (err) {
+    console.log(err?.data?.errors);
+    return { statusCode: 500, body: err.toString() };
+  }
 };
